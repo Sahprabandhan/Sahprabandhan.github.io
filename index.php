8b6bef9c3ca51a69fec62b0d7eb5f2ebff777532
Ravi <bhushanravi06@gmail.com>
2015-12-13 03:12:22 +0530

    Update - 1. Communication + Page Load

--- index.php
+++ index.php
@@ -13,6 +13,26 @@ and open the template in the editor.
         <meta name="keywords" content="Process Optimization, Development, NGO, NPO, Producer Company"/>
         <link rel="stylesheet" href="StyleSheets/cssIndex.css" type="text/css"/>
         <script>
+            var footerTimeout;
+            function toggleElementDisplay(elementID)
+            {
+                var temp = document.getElementById(elementID);
+                temp.style.display='block';
+                if(footerTimeout!==null)
+                    clearTimeout(footerTimeout);
+                footerTimeout=setTimeout(function(){temp.style.display="none";},5000);
+            }
+            function commonStyleProperties(node)
+            {
+                node.style.position="relative";
+                node.style.margin="5px auto";
+                node.style.boxShadow="0 8px 16px 0 rgba(228,108,10,0.2),0 16px 40px 0 rgba(228,108,10,0.19)";
+                node.style.alignItems="center";
+                node.style.display="none";
+//                node.style.borderStyle="solid";
+//                node.style.borderRadius="10px";
+//                node.style.borderColor="#e46c0a";
+            }
             function setElemColor(evt)
             {
                 evt.target.style.backgroundColor="whitesmoke";
@@ -48,155 +68,66 @@ and open the template in the editor.
                 xmlhttp.open("GET","Resources/Script/getInfo.php?infoID="+infoElement,true);
                 xmlhttp.send();
             }
+
             function navigate(elementID)
             {
                 var nodes = document.getElementById("DisplaySection"); //get the content display section
-                while(nodes.firstElementChild) //remove all currently displayed elements from the section
-                    nodes.removeChild(nodes.firstElementChild);
-                var temp = document.getElementById("NavBar").getBoundingClientRect().bottom+10;
-                nodes.style.top=temp.toString()+"px";
-                var node;
-                switch(elementID)
+                if(document.getElementById(elementID)===null)
                 {
-                    case "HomeImage": //to add homepage image
-                        node = document.createElement("img");
-                        node.id=elementID;
-                        node.src="ImageResources/BrandImage.png";
-                        node.alt="Brand Image";
-                        node.style.position="relative";
-                        node.style.top=(
+                    var temp = document.getElementById("NavBar").getBoundingClientRect().bottom;
+                    
+                    nodes.style.top=(temp+10).toString()+"px";
+                    
+                    var node;
+                    
+                    switch(elementID)
+                    {
+                        case "HomeImage"://Homepage
+                            node = document.createElement("img");
+                            node.id=elementID;
+                            node.src="ImageResources/BrandImage.png";
+                            node.alt="Brand Image";
+                            node.style.top=(
                                 //get available space in screen height after subtracting end of previous element
                                 (screen.height-
                                 document.getElementById("NavBar").getBoundingClientRect().bottom)/7//divide space in 7 parts
                                 ).toString()+"px";//start the top from end of 1st partition of height
-                        node.style.margin="5px auto";
-                        node.style.width=(screen.width/2).toString()+"px";
-                        node.style.boxShadow="0 4px 8px 0 rgba(0,0,0,0.2),0 8px 20px 0 rgba(0,0,0,0.19)";
-                        node.style.alignItems="center";
-                        node.style.display="block";
-                        node.style.borderStyle="solid";
-                        node.style.borderRadius="10px";
-                        node.style.borderColor="#e46c0a";
-                        nodes.appendChild(node);
-                        break;
-                    case "IntroNote":
-                        node = document.createElement("div");
-                        node.id=elementID;
-                        node.style.display="block";
-                        node.style.position="relative";
-                        node.style.top="0px";// (document.getElementById("NavBar").getBoundingClientRect().bottom+15).toString()+"px";//start the top from end of 1st partition of height
-                        node.style.margin="5px auto";
-                        node.style.width=document.getElementById("NavBar").width+"px";
-                        node.style.boxShadow="0 4px 8px 0 rgba(0,0,0,0.2),0 8px 20px 0 rgba(0,0,0,0.19)";
-                        node.style.alignItems="center";
-                        node.style.display="block";
-                        node.style.borderStyle="solid";
-                        node.style.borderRadius="10px";
-                        node.style.borderColor="#e46c0a";
-                        node.style.wordWrap="break-word";
-                        node.style.padding="10px";
-                        fetchInfo(elementID);
-                        nodes.appendChild(node);
-                        //node.innerHTML=
-                        break;
-                    case "TeamIntro":
-                        node = document.createElement("div");
-                        node.id=elementID;
-                        node.style.display="block";
-                        node.style.position="relative";
-                        node.style.top="0px"; //(document.getElementById("NavBar").getBoundingClientRect().bottom).toString()+"px";//start the top from end of 1st partition of height
-                        node.style.margin="0px auto";
-                        node.style.width=document.getElementById("NavBar").width+"px";
-                        node.style.boxShadow="0 4px 8px 0 rgba(0,0,0,0.2),0 8px 20px 0 rgba(0,0,0,0.19)";
-                        node.style.alignItems="center";
-                        node.style.display="block";
-                        node.style.borderStyle="solid";
-                        node.style.borderRadius="10px";
-                        node.style.borderColor="#e46c0a";
-                        node.style.wordWrap="break-word";
-                        node.style.padding="10px";
-                        fetchInfo(elementID);
-                        nodes.appendChild(node);
-                        //node.innerHTML=
-                        break;    
-                    case "Contact":
-                        node = document.createElement("img");
-                        node.id="SMS";
-                        node.src="ImageResources/Text.png";
-                        node.alt="Email";
-                        node.style.position="relative";
-                        node.style.top="0px";
-                        node.style.margin="5px auto";
-                        node.style.padding="5px";
-                        node.style.height="auto";
-                        node.style.boxShadow="0 4px 8px 0 rgba(0,0,0,0.2),0 8px 20px 0 rgba(0,0,0,0.19)";
-                        node.style.alignItems="center";
-                        node.style.display="block";
-                        node.style.borderStyle="solid";
-                        node.style.borderRadius="10px";
-                        node.style.borderColor="#e46c0a";
-                        node.addEventListener("mouseover",setElemColor,false);
-                        node.addEventListener("mouseout",resetElemColor,false);
-
-                        nodes.appendChild(node);
-                        node = document.createElement("img");
-                        node.id="Call";
-                        node.src="ImageResources/Phone.png";
-                        node.alt="Email";
-                        node.style.position="relative";
-                        node.style.top="0px";
-                        node.style.margin="5px auto";
-                        node.style.padding="5px";
-                        node.style.width="auto";
-                        node.style.boxShadow="0 4px 8px 0 rgba(0,0,0,0.2),0 8px 20px 0 rgba(0,0,0,0.19)";
-                        node.style.alignItems="center";
-                        node.style.display="block";
-                        node.style.borderStyle="solid";
-                        node.style.borderRadius="10px";
-                        node.style.borderColor="#e46c0a";
-                        node.addEventListener("mouseover",setElemColor,false);
-                        node.addEventListener("mouseout",resetElemColor,false);
-                        nodes.appendChild(node);
-                        node = document.createElement("img");
-                        node.id="Email";
-                        node.src="ImageResources/MailIcon.png";
-                        node.alt="EMail";
-                        node.style.position="relative";
-                        node.style.top="0px";
-                        node.style.margin="5px auto";
-                        node.style.padding="5px";
-                        node.style.height="auto";
-                        node.style.boxShadow="0 4px 8px 0 rgba(0,0,0,0.2),0 8px 20px 0 rgba(0,0,0,0.19)";
-                        node.style.alignItems="center";
-                        node.style.display="block";
-                        node.style.borderStyle="solid";
-                        node.style.borderRadius="10px";
-                        node.style.borderColor="#e46c0a";
-                        node.style.innerHTML="abc";
-                        nodes.appendChild(node);
-                        node.addEventListener("mouseover",setElemColor,false);
-                        node.addEventListener("mouseout",resetElemColor,false);
-
-                        node = document.createElement("pre");
-                        node.id="ContactValue";
-                        node.style.position="relative";
-                        node.style.top="10px";
-                        node.style.margin="5px auto";
-                        node.style.padding="5px";
-                        node.style.width="500px";
-                        node.style.minHeight="0px";
-                        node.style.height="auto";
-                        node.style.boxShadow="0 4px 8px 0 rgba(0,0,0,0.2),0 8px 20px 0 rgba(0,0,0,0.19)";
-                        node.style.alignItems="center";
-                        node.style.display="block";
-                        node.style.borderStyle="solid";
-                        node.style.borderRadius="10px";
-                        node.style.borderColor="#e46c0a";
-                        node.style.wordWrap="break-word";
-                        nodes.appendChild(node);
-                        break;
+                            node.style.width=(screen.width/2).toString()+"px";
+                            commonStyleProperties(node); //set common styles
+                            nodes.appendChild(node); //add node to DisplaySection
+                            break;
+                        case "IntroNote"://Introduction
+                            node = document.createElement("div");
+                            node.id=elementID;             
+                            node.style.top="0px";
+                            node.style.width=document.getElementById("NavBar").width+"px";
+                            node.style.wordWrap="break-word";
+                            node.style.padding="10px";
+                            commonStyleProperties(node);
+                            fetchInfo("IntroNote");
+                            nodes.appendChild(node);
+                            break;
+                        case "TeamIntro"://The Team
+                            node = document.createElement("div");
+                            node.id=elementID;
+                            node.style.top="0px"; //(document.getElementById("NavBar").getBoundingClientRect().bottom).toString()+"px";//start the top from end of 1st partition of height
+                            node.style.margin="0px auto";
+                            node.style.width=document.getElementById("NavBar").width+"px";
+                            node.style.wordWrap="break-word";
+                            node.style.padding="10px";
+                            commonStyleProperties(node);
+                            fetchInfo("TeamIntro");
+                            nodes.appendChild(node);
+                            break;
+                    }
                 }
-                //nodes.appendChild(node);
+                //Search child modes whether child element to be displayed exists.              
+                for(var i=0;i<nodes.childElementCount;i++)
+                    if(nodes.childNodes[i].nodeType===1)
+                        if(nodes.childNodes[i].id !== elementID)
+                            nodes.childNodes[i].style.display="none"; //If child is not the element then do not display it. 
+                        else
+                            nodes.childNodes[i].style.display="block";//If child is the element requested, then display it.
             }
         </script>
     </head>
@@ -204,7 +135,7 @@ and open the template in the editor.
         <nav id="NavBar" class="cssNavigation cssBoxShadow" onload="">
             <ul class="cssNavigationUl">
                 <li class="cssNavigationUlLi">
-                    <img id="Home" class="cssNavigationUlLia" src="ImageResources/Home.png" alt="Home" onload="navigate('HomeImage')" onclick="navigate('HomeImage')"/>
+                    <img id="Home" class="cssNavigationUlLia" src="ImageResources/Home.png" alt="Home" onload="navigate('HomeImage');" onclick="navigate('HomeImage')"/>
                 </li>
                 <li class="cssNavigationUlLi">
                     <pre id="Intro" class="cssNavigationUlLia" onclick="navigate('IntroNote')">An Introduction</pre>
@@ -213,10 +144,25 @@ and open the template in the editor.
                     <pre class="cssNavigationUlLia" onclick="navigate('TeamIntro')">The Team</pre>
                 </li>
                 <li class="cssNavigationUlLiRt">
-                    <pre id="Contact" class="cssNavigationUlLiaRt" onclick="navigate('Contact')">Communications</pre>
+                    <pre id="Contact" class="cssNavigationUlLiaRt" onmouseover="toggleElementDisplay('CommChannels')">Communications</pre>
                 </li>
             </ul>
         </nav>
-        <section id="DisplaySection" style="position:relative"></section> 
+        <section id="DisplaySection" style="position:relative;"></section>
+        
+        <footer id="CommChannels" class="cssFooter cssBoxShadow" onmouseover="toggleElementDisplay(this.id);">
+            <pre id="Email" class="cssFooterPre">
+                <script>fetchInfo("Email");</script>
+            </pre>
+            <img src="ImageResources/MailIcon.png" class="cssFooterImg"/>
+            <pre id="SMS" class="cssFooterPre">
+                <script>fetchInfo("SMS");</script>
+            </pre>
+            <img src="ImageResources/Text.png" class="cssFooterImg"/>
+            <pre id="Call" class="cssFooterPre">
+                <script>fetchInfo("Call");</script>
+            </pre>
+            <img src="ImageResources/Phone.png" class="cssFooterImg"/>
+        </footer>
     </body>
 </html>
